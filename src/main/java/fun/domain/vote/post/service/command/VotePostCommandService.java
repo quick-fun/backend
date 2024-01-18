6f0660767b9547e37e656a3dce255721b52d844d
hyena <goatsfish@gmail.com>
2024-01-18 19:36:33 +0900

    feat: 새로운 투표 게시글 생성 및 저장 서비스 기능 개발

--- src/main/java/fun/domain/vote/post/service/command/VotePostCommandService.java
+++ src/main/java/fun/domain/vote/post/service/command/VotePostCommandService.java
@@ -1,18 +1,19 @@
 package fun.domain.vote.post.service.command;
 
-import fun.common.auth.AuthAccessToken;
+import fun.domain.vote.item.domain.VoteItem;
 import fun.domain.vote.post.domain.DueDate;
 import fun.domain.vote.post.domain.VoteAssignHostValidator;
 import fun.domain.vote.post.domain.VotePost;
 import fun.domain.vote.post.domain.VotePostCommandRepository;
-import fun.domain.vote.post.service.command.request.CreateVotePostRequest;
 import fun.domain.vote.post.domain.VoteTag;
 import fun.domain.vote.post.domain.VoteTagCommandRepository;
+import fun.domain.vote.post.service.command.request.CreateVotePostRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional
@@ -23,19 +24,30 @@ public class VotePostCommandService {
     private final VoteTagCommandRepository voteTagCommandRepository;
     private final VoteAssignHostValidator voteAssignHostValidator;
 
-    public Long createVotePost(final AuthAccessToken authAccessToken, final CreateVotePostRequest votePostRequest) {
+    public Long createVotePost(
+            final Long requestMemberId,
+            final CreateVotePostRequest votePostRequest
+    ) {
         final VoteTag findVoteTag = voteTagCommandRepository.getVoteTagById(votePostRequest.tagId());
-        final DueDate dueDate = new DueDate(votePostRequest.localDateTime());
-
-        final VotePost newVotePost = new VotePost(
-                votePostRequest.title(),
-                votePostRequest.content(),
-                dueDate,
-                findVoteTag,
-                Collections.emptyList()
+        final DueDate requestDueDate = new DueDate(votePostRequest.localDateTime());
+        final VotePost savedVotePost = votePostCommandRepository.save(
+                new VotePost(
+                        votePostRequest.title(),
+                        votePostRequest.content(),
+                        requestDueDate,
+                        findVoteTag
+                )
         );
-        newVotePost.assignHost(authAccessToken.memberId(), voteAssignHostValidator);
 
-        return votePostCommandRepository.save(newVotePost).getId();
+        savedVotePost.assignHost(requestMemberId, voteAssignHostValidator);
+        savedVotePost.addVoteItems(convertToVoteItems(votePostRequest.voteItemTitles()));
+
+        return savedVotePost.getId();
+    }
+
+    private List<VoteItem> convertToVoteItems(final List<String> voteItemTitles) {
+        return voteItemTitles.stream()
+                .map(VoteItem::new)
+                .collect(Collectors.toList());
     }
 }
