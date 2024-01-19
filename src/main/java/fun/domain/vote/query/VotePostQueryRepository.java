215e4b0c1f410b3f5a7eccb33c202884d1c1dffd
hyena <goatsfish@gmail.com>
2024-01-19 15:46:33 +0900

    chore: 조회 레포지터리 패키지 이동

--- src/main/java/fun/domain/vote/query/VotePostQueryRepository.java
+++
@@ -1,7 +0,0 @@
-package fun.domain.vote.query;
-
-import fun.domain.vote.post.domain.VotePost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VotePostQueryRepository extends JpaRepository<VotePost, Long> {
-}
--- src/main/java/fun/domain/vote/query/repository/MedalQueryRepository.java
+++ src/main/java/fun/domain/vote/query/repository/MedalQueryRepository.java
@@ -1,4 +1,4 @@
-package fun.domain.vote.query;
+package fun.domain.vote.query.repository;
 
 import fun.domain.medal.domain.Medal;
 import org.springframework.data.jpa.repository.JpaRepository;
