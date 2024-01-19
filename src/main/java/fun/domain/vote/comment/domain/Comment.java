484bc0d43c3fa82a145dfc761485bc1da976b49d
hyena <goatsfish@gmail.com>
2024-01-19 16:25:47 +0900

    feat: 댓글 엔티티 개발

---
+++ src/main/java/fun/domain/vote/comment/domain/Comment.java
@@ -0,0 +1,50 @@
+package fun.domain.vote.comment.domain;
+
+import fun.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String comment;
+
+    @Column(name = "vote_post_id", nullable = false)
+    private Long votePostId;
+
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    protected Comment() {
+    }
+
+    public Comment(
+            final String comment,
+            final Long votePostId,
+            final Long memberId
+    ) {
+        this(null, comment, votePostId, memberId);
+    }
+
+    protected Comment(
+            final Long id,
+            final String comment,
+            final Long votePostId,
+            final Long memberId
+    ) {
+        this.id = id;
+        this.comment = comment;
+        this.votePostId = votePostId;
+        this.memberId = memberId;
+    }
+}
