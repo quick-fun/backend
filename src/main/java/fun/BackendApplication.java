b52609852335814726f9e6325eba7f83bd4e8ed1
hyena <goatsfish@gmail.com>
2024-01-21 09:48:42 +0900

    feat: 애플리케이션 비동기 활성화 어노테이션 등록

--- src/main/java/fun/BackendApplication.java
+++ src/main/java/fun/BackendApplication.java
@@ -3,7 +3,9 @@ package fun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class BackendApplication {
